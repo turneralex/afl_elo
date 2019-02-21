49d61325410292aad394ce764090da5054382e92
turneralex <alexanderturner1991@gmail.com>
2019-02-21 21:40:53 +1100

    test scraping function for updating 2019 data.

--- elo_2019.R
+++ elo_2019.R
@@ -8,3 +8,22 @@ elo_model <- elo.run(
     k = elo_par["k"],
     data = afl_elo
 ) 
+
+get_round_scores("https://en.wikipedia.org/wiki/2018_AFL_season", round = 1)   
+
+afl_fixture_2019 %>% 
+    filter(round == "Round 1") %>% 
+    mutate()
+
+bind_rows(
+    afl_elo,
+    afl_fixture_2019 %>% 
+        filter(round == "Round 1") %>% 
+        mutate(
+            home_score = get_round_scores("https://en.wikipedia.org/wiki/2018_AFL_season", round = 1) %>% pull(home_score),
+            away_score = get_round_scores("https://en.wikipedia.org/wiki/2018_AFL_season", round = 1) %>% pull(away_score),
+            home_score_adjusted = home_score / (home_score + away_score),
+            hga_app = pmap_int(list(season, venue, away_team), is_home, data = afl_venues_all)
+        )
+) %>% 
+    filter(season == "2019")
