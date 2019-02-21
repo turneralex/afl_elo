43653e8afa24d111fa11fe6fdf7008efbd1463d6
turneralex <alexanderturner1991@gmail.com>
2019-02-21 19:44:11 +1100

    add function for scraping 2019 scores round by round

--- functions_general.R
+++ functions_general.R
@@ -23,21 +23,10 @@ fixture_url_2019 <- "https://en.wikipedia.org/wiki/2019_AFL_season"
 
 round_1 <- 3
 
-fixture_url_2018 %>% 
-    read_html() %>% 
-    html_nodes(xpath = paste("//*[@id='mw-content-text']/div/table[", "3", "]")) %>% 
-    html_table() %>% 
-    magrittr::extract2(1) %>% 
-    as_tibble() %>% 
-    filter(X3 %>% str_detect("def\\.|def\\. by|drew with")) %>% 
-    select(X2, X4) %>% 
-    transmute(home_score = X2 %>% str_remove_all(".*[(]|[)]") %>% as.integer(),
-              away_score = X4 %>% str_remove_all(".*[(]|[)]") %>% as.integer())
-
 get_round_scores <- function(fixture_url, table_id) {
-    fixture_url_2018 %>% 
+    fixture_url %>% 
         read_html() %>% 
-        html_nodes(xpath = paste("//*[@id='mw-content-text']/div/table[", "3", "]")) %>% 
+        html_nodes(xpath = paste("//*[@id='mw-content-text']/div/table[", table_id, "]")) %>% 
         html_table() %>% 
         magrittr::extract2(1) %>% 
         as_tibble() %>% 
@@ -47,4 +36,4 @@ get_round_scores <- function(fixture_url, table_id) {
                   away_score = X4 %>% str_remove_all(".*[(]|[)]") %>% as.integer())
 }
 
-get_round_scores(fixture_url_2019, )    
+get_round_scores(fixture_url_2019, round_1 + 1)    
