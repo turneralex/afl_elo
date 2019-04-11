06cc673ed4ccb5c5b218c8b9fd2e1898defbe096
turneralex <alexanderturner1991@gmail.com>
2019-04-11 16:51:11 +1000

    round 4 tips.

--- elo_2019.R
+++ elo_2019.R
@@ -8,13 +8,15 @@ afl_fixture_2019 <- afl_fixture_2019 %>%
         home_score = replace(home_score, .$round == "Round 1", get_round_scores(2019, round = 1) %>% pull(home_score)),
         away_score = replace(away_score, .$round == "Round 1", get_round_scores(2019, round = 1) %>% pull(away_score)),
         home_score = replace(home_score, .$round == "Round 2", get_round_scores(2019, round = 2) %>% pull(home_score)),
-        away_score = replace(away_score, .$round == "Round 2", get_round_scores(2019, round = 2) %>% pull(away_score))
+        away_score = replace(away_score, .$round == "Round 2", get_round_scores(2019, round = 2) %>% pull(away_score)),
+        home_score = replace(home_score, .$round == "Round 3", get_round_scores(2019, round = 3) %>% pull(home_score)),
+        away_score = replace(away_score, .$round == "Round 3", get_round_scores(2019, round = 3) %>% pull(away_score))
     )
 
 afl_elo <- bind_rows(
     afl_elo,
     afl_fixture_2019 %>% 
-        filter(round %in% map_chr(1:2, ~ paste("Round", .))) %>% 
+        filter(round %in% map_chr(1:3, ~ paste("Round", .))) %>% # update this
         mutate(home_score_adjusted = home_score / (home_score + away_score),
                hga_app = pmap_int(list(season, venue, away_team), is_home, data = afl_venues_all))
 ) 
@@ -32,12 +34,12 @@ elo_model %>%
 elo_model %>% 
     predict(
         newdata = afl_fixture_2019 %>% 
-            filter(round == "Round 3") %>% 
+            filter(round == "Round 4") %>% 
             mutate(hga_app = pmap_int(list(season, venue, away_team), is_home, data = afl_venues_all))
     ) %>% 
     set_names(
         afl_fixture_2019 %>% 
-            filter(round == "Round 3") %>% 
+            filter(round == "Round 4") %>% 
             pull(home_team)
     )
 
