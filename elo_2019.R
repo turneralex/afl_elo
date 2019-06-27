82596f93e3974cc09ce12a04e87904c6b83a9b9b
turneralex <alexanderturner1991@gmail.com>
2019-06-27 10:49:12 +1000

    simplify updating 2019 score data.

--- elo_2019.R
+++ elo_2019.R
@@ -3,42 +3,26 @@ source("./functions_general.R")
 library(tidyverse)
 library(elo)
 
-afl_fixture_2019 <- afl_fixture_2019 %>% 
-    mutate(
-        home_score = replace(home_score, .$round == "Round 1", get_round_scores(2019, round = 1) %>% pull(home_score)),
-        away_score = replace(away_score, .$round == "Round 1", get_round_scores(2019, round = 1) %>% pull(away_score)),
-        home_score = replace(home_score, .$round == "Round 2", get_round_scores(2019, round = 2) %>% pull(home_score)),
-        away_score = replace(away_score, .$round == "Round 2", get_round_scores(2019, round = 2) %>% pull(away_score)),
-        home_score = replace(home_score, .$round == "Round 3", get_round_scores(2019, round = 3) %>% pull(home_score)),
-        away_score = replace(away_score, .$round == "Round 3", get_round_scores(2019, round = 3) %>% pull(away_score)),
-        home_score = replace(home_score, .$round == "Round 4", get_round_scores(2019, round = 4) %>% pull(home_score)),
-        away_score = replace(away_score, .$round == "Round 4", get_round_scores(2019, round = 4) %>% pull(away_score)),
-        home_score = replace(home_score, .$round == "Round 5", get_round_scores(2019, round = 5) %>% pull(home_score)),
-        away_score = replace(away_score, .$round == "Round 5", get_round_scores(2019, round = 5) %>% pull(away_score)),
-        home_score = replace(home_score, .$round == "Round 6", get_round_scores(2019, round = 6) %>% pull(home_score)),
-        away_score = replace(away_score, .$round == "Round 6", get_round_scores(2019, round = 6) %>% pull(away_score)),
-        home_score = replace(home_score, .$round == "Round 7", get_round_scores(2019, round = 7) %>% pull(home_score)),
-        away_score = replace(away_score, .$round == "Round 7", get_round_scores(2019, round = 7) %>% pull(away_score)),
-        home_score = replace(home_score, .$round == "Round 8", get_round_scores(2019, round = 8) %>% pull(home_score)),
-        away_score = replace(away_score, .$round == "Round 8", get_round_scores(2019, round = 8) %>% pull(away_score)),
-        home_score = replace(home_score, .$round == "Round 9", get_round_scores(2019, round = 9) %>% pull(home_score)),
-        away_score = replace(away_score, .$round == "Round 9", get_round_scores(2019, round = 9) %>% pull(away_score)),
-        home_score = replace(home_score, .$round == "Round 10", get_round_scores(2019, round = 10) %>% pull(home_score)),
-        away_score = replace(away_score, .$round == "Round 10", get_round_scores(2019, round = 10) %>% pull(away_score)),
-        home_score = replace(home_score, .$round == "Round 11", get_round_scores(2019, round = 11) %>% pull(home_score)),
-        away_score = replace(away_score, .$round == "Round 11", get_round_scores(2019, round = 11) %>% pull(away_score)),
-        home_score = replace(home_score, .$round == "Round 12", get_round_scores(2019, round = 12) %>% pull(home_score)),
-        away_score = replace(away_score, .$round == "Round 12", get_round_scores(2019, round = 12) %>% pull(away_score)),
-        home_score = replace(home_score, .$round == "Round 13", get_round_scores(2019, round = 13) %>% pull(home_score)),
-        away_score = replace(away_score, .$round == "Round 13", get_round_scores(2019, round = 13) %>% pull(away_score)),
-        home_score = replace(home_score, .$round == "Round 14", get_round_scores(2019, round = 14) %>% pull(home_score)),
-        away_score = replace(away_score, .$round == "Round 14", get_round_scores(2019, round = 14) %>% pull(away_score))
+rounds_so_far <- 1:14
+
+afl_fixture_2019 <- map_dfr(
+    rounds_so_far,
+    ~ afl_fixture_2019 %>% 
+        filter(round == paste("Round", .x)) %>% 
+        mutate(
+            home_score = replace(home_score, .$round == paste("Round", .x), get_round_scores(2019, round = .x) %>% pull(home_score)),
+            away_score = replace(away_score, .$round == paste("Round", .x), get_round_scores(2019, round = .x) %>% pull(away_score))
+        )
+) %>% 
+    bind_rows(
+        afl_fixture_2019 %>% 
+            filter(!(round %in% paste("Round", rounds_so_far)))
     )
 
 afl_elo <- bind_rows(
     afl_elo,
     afl_fixture_2019 %>% 
-        filter(round %in% paste("Round", 1:14)) %>% # update this
+        filter(round %in% paste("Round", rounds_so_far)) %>% 
         mutate(home_score_adjusted = home_score / (home_score + away_score),
                hga_app = pmap_int(list(season, venue, away_team), is_home, data = afl_venues_all))
 ) 
@@ -65,6 +49,3 @@ elo_model %>%
             filter(round == "Round 15") %>% 
             pull(home_team)
     )
-
-
-
