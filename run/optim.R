56089896dc1df6ebfe526070cbedd8d0bb9e9333
Alex Turner <alexanderturner1991@gmail.com>
2022-09-11 11:15:22 +1000

    repo clean

--- run/optim.R
+++ run/optim.R
@@ -1,9 +1,18 @@
-source(here::here("fixture scripts/fixture_all.R"))
-source(here::here("elo update/functions_general.R"))
+source(here::here("utils/fixture scripts/fixture_all.R"))
+source(here::here("utils/functions_general.R"))
 
 library(tidyverse)
 
+seasons_exclude <- c(
+    "2010", 
+    "2020", 
+    "2021"
+)
+
 afl_elo <- afl_fixture_all %>% 
+    filter(
+        !(season %in% seasons_exclude)
+    ) %>%
     convert_elo_df()
 
 parameter_optim <- function(elo_df, par) {
@@ -21,7 +30,7 @@ parameter_optim <- function(elo_df, par) {
         next_season <- elo_df %>% 
             slice(row_id:nrow(.)) %>% 
             group_by(team) %>% 
-            mutate(season = lead(season, default = "2019")) %>% 
+            mutate(season = lead(season, default = "9999")) %>% 
             filter(team == game[1, "team"]) %>% 
             pull(season) %>% 
             magrittr::extract2(1)
@@ -32,9 +41,29 @@ parameter_optim <- function(elo_df, par) {
             regress_app <- 1
         }
         
-        score_expected_1 = score_expected(game[1, "start_elo"], game[2, "start_elo"], hga_app = game[1, "hga_app"], hga = par[2])
+        location <- game$location[1]
         
-        score_expected_2 = score_expected(game[2, "start_elo"], game[1, "start_elo"], hga_app = game[2, "hga_app"], hga = par[2])
+        if (location == "VIC") {
+            hga <- par[3]
+        } else if (location == "NSW") {
+            hga <- par[4]
+        } else if (location == "QLD") {
+            hga <- par[5]
+        } else if (location == "SA") {
+            hga <- par[6]
+        } else if (location == "WA") {
+            hga <- par[7]
+        } else if (location == "GEE") {
+            hga <- par[8]
+        } else if (location == "TAS") {
+            hga <- par[9]
+        } else {
+            hga <- par[10]
+        }
+        
+        score_expected_1 = score_expected(game[1, "start_elo"], game[2, "start_elo"], hga_app = game[1, "hga_app"], hga = hga)
+        
+        score_expected_2 = score_expected(game[2, "start_elo"], game[1, "start_elo"], hga_app = game[2, "hga_app"], hga = hga)
         
         elo_df[elo_df$match_id == i, "score_expected"] <- c(score_expected_1, score_expected_2) %>%
             unlist()
@@ -45,7 +74,7 @@ parameter_optim <- function(elo_df, par) {
             score_adjusted = game[1, "score_adjusted"],
             score_expected = score_expected_1,
             k = par[1],
-            regress = par[3],
+            regress = par[2],
             regress_app = regress_app
         )
         
@@ -55,7 +84,7 @@ parameter_optim <- function(elo_df, par) {
             score_adjusted = game[2, "score_adjusted"],
             score_expected = score_expected_2,
             k = par[1],
-            regress = par[3],
+            regress = par[2],
             regress_app = regress_app
         )
         
@@ -77,15 +106,17 @@ parameter_optim <- function(elo_df, par) {
 }
 
 elo_par <- optim(
-    par = c(50, 50, 0.5),
-    lower = c(-Inf, -Inf, 0),
-    upper = c(Inf, Inf, 1),
+    par = c(50, 0.5, rep(50, 8)),
+    lower = rep(0, 11),
+    upper = c(100, 1, rep(100, 8)),
     parameter_optim, 
     elo_df = afl_elo,
     method = "L-BFGS-B"
 ) %>% 
     purrr::pluck("par") %>% 
-    set_names(c("k", "hga", "regress")) %>% 
+    set_names(c("k", "regress", "hga_vic", "hga_nsw", "hga_qld", "hga_sa", "hga_wa", "hga_gee", "hga_tas", "hga_other"))
+
+elo_par %>%     
     enframe() %>% 
     rename(param = name) %>% 
-    write_csv(here::here("new elo", "elo_par.csv"))
+    write_csv(here::here("params", "elo_par_score_shots_2022.csv"))
